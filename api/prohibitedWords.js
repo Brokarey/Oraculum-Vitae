7f8c29c4c237a73b298038090af477696e21a6ef
Guilherme Sampaio <guilhermesampaio1808@hotmail.com>
2026-09-19 01:32:33 -0300

    Add prohibitedWords.js

---
+++ api/prohibitedWords.js
@@ -0,0 +1,9 @@
+// api/prohibitedWords.js
+module.exports.PALAVRAS_PROIBIDAS = [
+  // ---- Violência ----
+  'matar', 'assassinato', 'agredir', 'espancar', 'golpear',
+  // ---- Insultos / baixo calão ----
+  'porra', 'caralho', 'filho da puta', 'buceta', 'pinto',
+  // ---- Discurso de ódio (exemplo) ----
+  'racista', 'racismo', 'homofóbico', 'homofobia'
+];
